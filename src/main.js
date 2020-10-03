cea31d61db3b4fd89a9e0b2392b4b461dd7e0ce3
deeraj <deerajn04@gmail.com>
2020-10-04 00:50:56 +0530

    fixed the bug of scripts

--- src/main.js
+++ src/main.js
@@ -1,40 +1,17 @@
-//electron namespaces
 const { app, BrowserWindow } = require("electron");
 
-//function to create window and return the object
-
-/*
-
-:param width : width of the window
-:type width : integer  || 400
-
-:param height : height of the window
-:type height : integer  || 800
-
-
-
-:param title : title of the window 
-:type title : "string" || new_window
-
-return  BrowserWindow || undefined 
-*/
-function createWindow(
-  url = undefined,
-  width = 800,
-  height = 400,
-  title = "new_window"
-) {
-  let win;
-  win = new BrowserWindow({
-    width,
-    height,
-    title,
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
   });
-  try {
-    win.BrowserWindow(url);
-    return win;
-  } catch (err) {
-    console.error("could not create a window, May be the invalid URL");
-  }
-  return undefined;
+
+  // and load the index.html of the app.
+  win.loadFile("./html/index.html");
 }
+
+app.whenReady().then(createWindow);
